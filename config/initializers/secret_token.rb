c7c91590b3ee141a3af40b31bc32701bd8c06068
Anderson Parra <ander.pp@gmail.com>
2013-03-18 12:46:05 -0300

    ínit project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoplet::Application.config.secret_token = 'f19e82a188e4a86dbbffbd0f7d5bcfed83c09a69378f686b68c6c345f414c48ec321895408a2647c4a36f43bcaf99c1537973c31729a561b32ee64e6fa88392b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shoplet::Application.config.session_store :cookie_store, key: '_shoplet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shoplet::Application.config.session_store :active_record_store
